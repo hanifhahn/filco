1a2ec181eaf6fcb376e8d96bc14209301db2e4f0
poppyma <95040717+poppyma@users.noreply.github.com>
2023-05-31 06:08:36 +0700

    Update SearchData.kt

--- app/src/main/java/com/example/filmopo/data/api/SearchData.kt
+++ app/src/main/java/com/example/filmopo/data/api/SearchData.kt
@@ -5,4 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class SearchData(
     @SerializedName("Search") val data:List<MovieData>,
     @SerializedName("totalResults") val totalData:Int,
+    @SerializedName("Response") val res:String,
 )
