503021b301a015de32437c9ff1bfe4ac9eb68c6d
poppyma <95040717+poppyma@users.noreply.github.com>
2023-05-31 06:07:53 +0700

    Update MovieViewModel.kt

--- app/src/main/java/com/example/filmopo/data/api/MovieViewModel.kt
+++ app/src/main/java/com/example/filmopo/data/api/MovieViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.filmopo.data.api
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -9,6 +11,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.filmopo.data.Constant
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,49 +20,116 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MovieViewModel(private val stateHandle: SavedStateHandle) : ViewModel(){
-    private var restInterface: ApiService
+class MovieViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
+    private val restInterface: ApiService
     val state = mutableStateOf(emptyList<MovieData>())
-    private lateinit var movieCall: Call<SearchData>
-    val list = ArrayList<MovieData>()
 
     init {
         val retrofit: Retrofit = Retrofit.Builder()
-            .addConverterFactory(
-                GsonConverterFactory.create()
-            )
-            .baseUrl(
-                RClient.BASE_URL
-            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("http://www.omdbapi.com/")
             .build()
-        restInterface = retrofit.create(
-            ApiService::class.java
-        )
-        getListMovie()
+        restInterface = retrofit.create(ApiService::class.java)
+        getListMovie("s")
     }
 
-    private fun getListMovie(){
-        movieCall = restInterface.getMovies()
-        movieCall.enqueue(
-            object : Callback<SearchData> {
-                override fun onResponse(call: Call<SearchData>, response: Response<SearchData>) {
-                    response.body()?.let { list.addAll(it.data)
-                        state.value = it.data
+    private fun getListMovie(query: String) {
+        val call = restInterface.getMovies(query, "51f05115")
+        call.enqueue(object : Callback<SearchData> {
+            override fun onResponse(call: Call<SearchData>, response: Response<SearchData>) {
+                if (response.isSuccessful) {
+                    val searchData = response.body()
+                    searchData?.let {
+                        state.value = searchData.data ?: emptyList()
                     }
+                } else {
+                    Log.e("MovieViewModel", "Error: ${response.message()}")
                 }
-                override fun onFailure(call: Call<SearchData>, t: Throwable) {
-                    t.printStackTrace()
-                }
-            })
+            }
+
+            override fun onFailure(call: Call<SearchData>, t: Throwable) {
+                Log.e("MovieViewModel", "Error: ${t.message}")
+            }
+        })
     }
 
-//    fun getDetailMovie(imdbID: Int){
-//        viewModelScope.launch {
-//            try {
-//                movie = listMovie.first{it.imdbID == imdbID}
-//            } catch (e: Exception){
-//                Log.d("error:", e.message.toString())
-//            }
-//        }
-//    }
+    fun searchMovies(query: String, applicationContext: Context) {
+        val call = restInterface.getMovies(query, "51f05115")
+        call.enqueue(object : Callback<SearchData> {
+            override fun onResponse(call: Call<SearchData>, response: Response<SearchData>) {
+                val searchData = response.body()
+                val res = searchData?.res
+
+                if (res == "True") {
+                    searchData?.let {
+                        state.value = searchData.data ?: emptyList()
+                    }
+                } else {
+                    Toast.makeText(applicationContext, "Movie Not Found", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<SearchData>, t: Throwable) {
+                Log.e("MovieViewModel", "Error: ${t.message}")
+            }
+        })
+    }
+
+    suspend fun getMovieById(movieId: String): MovieDetailData? {
+        val deferred = CompletableDeferred<MovieDetailData?>()
+        val call = restInterface.getDetailMovies(movieId, "51f05115")
+        call.enqueue(object : Callback<MovieDetailData> {
+            override fun onResponse(call: Call<MovieDetailData>, response: Response<MovieDetailData>) {
+                if (response.isSuccessful) {
+                    val movieDetailData = response.body()
+                    movieDetailData?.let {
+                        deferred.complete(movieDetailData)
+                    } ?: run {
+                        deferred.complete(null)
+                    }
+                } else {
+                    Log.e("MovieViewModel", "Error: ${response.message()}")
+                    deferred.complete(null)
+                }
+            }
+
+            override fun onFailure(call: Call<MovieDetailData>, t: Throwable) {
+                Log.e("MovieViewModel", "Error: ${t.message}")
+                deferred.complete(null)
+            }
+        })
+        return deferred.await()
+    }
+
+
+
+    private suspend fun getAdditionalMovieData(movieId: String): MovieDetailData? {
+        val deferred = CompletableDeferred<MovieDetailData?>()
+        val call = restInterface.getDetailMovies(movieId, "51f05115")
+        call.enqueue(object : Callback<MovieDetailData> {
+            override fun onResponse(call: Call<MovieDetailData>, response: Response<MovieDetailData>) {
+                if (response.isSuccessful) {
+                    val movieDetailData = response.body()
+                    movieDetailData?.let {
+                        deferred.complete(movieDetailData)
+                    } ?: run {
+                        deferred.complete(null)
+                    }
+                } else {
+                    Log.e("MovieViewModel", "Error: ${response.message()}")
+                    deferred.complete(null)
+                }
+            }
+
+            override fun onFailure(call: Call<MovieDetailData>, t: Throwable) {
+                Log.e("MovieViewModel", "Error: ${t.message}")
+                deferred.complete(null)
+            }
+        })
+        return deferred.await()
+    }
+
+
 }
+
+
