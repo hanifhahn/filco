2b4e0126244e9effdce8d530ec87e2da19cb06ac
poppyma <95040717+poppyma@users.noreply.github.com>
2023-05-31 06:08:51 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/filmopo/data/api/MovieDetailData.kt
@@ -0,0 +1,12 @@
+package com.example.filmopo.data.api
+
+import android.media.Image
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MovieDetailData(
+    val Title: String,
+    val Year: String,
+    val Poster: String,
+    val Released:String,
+)
+
