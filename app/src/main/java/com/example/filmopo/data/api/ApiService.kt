e969599034990962b055af758b3f5323b6c336b9
poppyma <95040717+poppyma@users.noreply.github.com>
2023-05-31 06:06:11 +0700

    Update ApiService.kt

--- app/src/main/java/com/example/filmopo/data/api/ApiService.kt
+++ app/src/main/java/com/example/filmopo/data/api/ApiService.kt
@@ -2,10 +2,20 @@ package com.example.filmopo.data.api
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("?s=spider&apikey=51f05115")
+    @GET("/")
+    fun getMovies(
+        @Query("s") s: String?,
+        @Query("apikey") apikey: String
+    ): Call<SearchData>
 
-    fun getMovies() : Call<SearchData>
+    @GET("/")
+    fun getDetailMovies(
+        @Query("i") i: String?,
+        @Query("apikey") apikey: String
+    ): Call<MovieDetailData>
 }
+
 
