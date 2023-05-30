67ccca13d32c351beb2e46d8d0e991ede061bd75
poppyma <95040717+poppyma@users.noreply.github.com>
2023-05-31 06:09:59 +0700

    Update Constant.kt

--- app/src/main/java/com/example/filmopo/data/Constant.kt
+++ app/src/main/java/com/example/filmopo/data/Constant.kt
@@ -1,9 +1,11 @@
 package com.example.filmopo.data
 
+import com.example.filmopo.data.api.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object Constant {
     const val ServerClient = "1040331464237-nmtl0v51a93kdgej24j15i69ilcskbi6.apps.googleusercontent.com"
-    const val OMDB_BASE_URL = "https://www.omdbapi/.com"
-    const val OMDB_API_KEY = "4361a955"
-
-//    const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    const val OMDB_BASE_URL = "http://www.omdbapi.com/"
+    const val OMDB_API_KEY = "51f05115"
 }
