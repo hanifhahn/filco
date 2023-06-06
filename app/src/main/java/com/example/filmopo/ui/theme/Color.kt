f2d54062da6443be15732822d269980a88f1132d
poppyma <95040717+poppyma@users.noreply.github.com>
2023-06-06 08:07:20 +0700

    Update Color.kt

--- app/src/main/java/com/example/filmopo/ui/theme/Color.kt
+++ app/src/main/java/com/example/filmopo/ui/theme/Color.kt
@@ -6,6 +6,11 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val deepPurple = Color(0xFF1B1464)
 
 val lightBlue = Color(0xffd8e6ff)
 val blue = Color(0xff76a9ff)
+val darkGray = Color(0xFF191D20)
+val brightRed = Color(0xFFEF233C)
+
+val Gray = Color(0xFF272A2F)
