119855409d7ab908a263b9e4dc635e2baee027e0
poppyma <95040717+poppyma@users.noreply.github.com>
2023-06-06 07:22:04 +0700

    Update SignInScreen.kt

--- app/src/main/java/com/example/filmopo/presentation/login_screen/SignInScreen.kt
+++ app/src/main/java/com/example/filmopo/presentation/login_screen/SignInScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -103,7 +104,8 @@ fun SignInScreen(
                 focusedIndicatorColor = Color.Transparent
             ), shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
                 Text(text = "Password")
-            }
+            },
+            visualTransformation = PasswordVisualTransformation()
         )
         Button(
             onClick = {
