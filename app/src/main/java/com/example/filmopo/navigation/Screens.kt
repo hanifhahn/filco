79c16888d7e61d2d960bad2f9a91aeeb21cb7c2d
poppyma <95040717+poppyma@users.noreply.github.com>
2023-06-06 07:19:51 +0700

    Update Screens.kt

--- app/src/main/java/com/example/filmopo/navigation/Screens.kt
+++ app/src/main/java/com/example/filmopo/navigation/Screens.kt
@@ -1,6 +1,8 @@
 package com.example.filmopo.navigation
 
 sealed class Screens(val route: String) {
+    object SplashScreen : Screens(route = "SplashScreen")
+    object OnBoardingFirst : Screens(route = "OnBoardingFirst")
     object SignInScreen : Screens(route = "SignIn_Screen")
     object SignUpScreen : Screens(route = "SignUp_Screen")
     object HomepageScreen : Screens(route = "HomepageScreen")
